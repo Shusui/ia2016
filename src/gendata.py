be27b74f589a66b5685870bcf4f61f2ae9d48757
Shusui <wir0.gongasgreen@gmail.com>
2015-12-07 23:10:39 +0000

    Optimized (?)

--- src/gendata.py
+++ src/gendata.py
@@ -27,13 +27,13 @@ def gen(perc):
 		aux = info[0:len(info)-1].split(',')
 		to_shuffle.append([int(aux[0].split('.')[0]) - 1, float(aux[1])])
 		img = io.imread(os.getcwd() + '/Data/' + aux[0], True)
-		data.append(img)
 		max_h = max(max_h, img.shape[0])
 		max_w = max(max_w, img.shape[1])
 	print "Finished loading images..."
 
-	for img in data:
-		img = resize(img, (max_h, max_w)))
+	for i in to_shuffle:
+		img = io.imread(os.getcwd() + '/Data/' + str(i[0] + 1) + '.jpg', True)
+		img = resize(img, (max_h, max_w))
 		img = img.ravel()
 		new_data.append(img)
 	print "All images normalized..."
@@ -42,17 +42,17 @@ def gen(perc):
 	random.shuffle(to_shuffle)
 	ind = int(floor(len(to_shuffle)*perc))
 	print "Geting train and test data..."
-	print len(data), len(to_shuffle)
 
 	for i in range(0, ind):
-		print i, to_shuffle[i][0]
 		train_data.append(data[to_shuffle[i][0]])
 		train_target.append(create_target(to_shuffle[i][1]))
 	for i in range(ind, len(to_shuffle)):
-		print i, to_shuffle[i][0]
 		test_data.append(data[to_shuffle[i][0]])
 		test_target.append(create_target(to_shuffle[i][1]))
 	print "Ready for classification..."
 
 	output = [train_data, train_target, test_data, test_target]
 	return output
+
+if __name__ == "__main__":
+	gen(0.7)
