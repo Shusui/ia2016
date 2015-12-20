56c108b8708d0d1a74cc3bc6bc902bfa214c3b5e
rgoomes <ricardo.jpg@hotmail.com>
2015-12-20 16:18:24 +0000

    Keras framework

--- src/gendata.py
+++ src/gendata.py
@@ -1,52 +1,66 @@
-import numpy, os, random
-from math import floor
+import numpy, os, random, sys
+
+from math import *
 from skimage import io
 from skimage.transform import resize
 
 def target(score):
-	t = numpy.zeros(10)
-	t[int(floor(score * 10))] = 1
+	scores = numpy.arange(0.3, 1.01, 0.1)
+	t = numpy.zeros(len(scores))
+
+	for i in range(len(scores)):
+		if score <= scores[i]:
+			t[i] = 1
+			break;
+
 	return t
 
-def gen(perc):
+def simplify(img):
+	for i in range(len(img)):
+		img[i] = int(255 * img[i]) / 255.0
+
+	return img
+
+def get_path(img_line):
+	return 'BigData/' + img_line[1] + '/' + img_line[2]
+def get_memorability(img_line):
+	return float(img_line[3]) / (float(img_line[3]) + float(img_line[4]))
+
+def gen(perc, max_images):
 
 	data = []
-	to_shuffle = []
+	helper = []
 	train_data = []
 	train_target = []
 	test_data = []
 	test_target = []
 
-	max_images = 4
-
-	print "Loading images.."
-	with open('scores.txt') as f:
+	with open('BigData/memorabiliy.csv') as f:
 		content = f.readlines()
 
-	content = content[:max_images]
+	content = content[1:min(len(content), max_images)]
 
+	print 'Loading images..'
 	for info in content:
 		aux = info.rstrip('\n').split(',')
-		to_shuffle.append([int(aux[0][:-4])-1, float(aux[1])])
+		helper.append([int(aux[0])-1, get_memorability(aux), aux[1]+'/'+aux[2]])
 
-		img = io.imread('Data/' + aux[0], as_grey=True)
-		img = resize(img, (16,16))
+		img = io.imread(get_path(aux), as_grey=True)
+		img = resize(img, (128,128))
 		img = img.ravel()
+		img = simplify(img)
 
 		data.append(img)
-	print "All images loaded and normalized.."
 
-	random.shuffle(to_shuffle)
-	ind = int(floor(len(to_shuffle)*perc))
-
-	print "Getting train and test data.."
+	print 'Image loading finished. Generating train and test data..'
+	random.shuffle(helper)
+	ind = int(floor(len(helper)*perc))
 
 	for i in range(ind):
-		train_data.append(data[to_shuffle[i][0]])
-		train_target.append(target(to_shuffle[i][1]))
-	for i in range(ind, len(to_shuffle)):
-		test_data.append(data[to_shuffle[i][0]])
-		test_target.append(target(to_shuffle[i][1]))
-	print "Ready for classification.."
+		train_data.append(data[helper[i][0]])
+		train_target.append(target(helper[i][1]))
+	for i in range(ind, len(helper)):
+		test_data.append(data[helper[i][0]])
+		test_target.append(target(helper[i][1]))
 
-	return [train_data, train_target, test_data, test_target, to_shuffle]
+	return [train_data, train_target, test_data, test_target, helper]
---
+++ src/new_classifier.py
@@ -0,0 +1,74 @@
+from __future__ import print_function
+from gendata import *
+
+import numpy, sys
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD, Adam, RMSprop
+from keras.utils import np_utils
+
+def ConvolutionalNeuralNetwork(train_data, train_target, test_data, test_target):
+	print('cnn')
+	
+def MultiLayerPerceptron(train_data, train_target, test_data, test_target):
+	numpy.random.seed(1337)
+
+	epoch  = 10
+	batch  = 128
+	layers = 512
+
+	n_ins  = len(train_data[0])
+	n_out  = len(train_target[0])
+
+	model = Sequential()
+	model.add(Dense(layers, input_shape=(n_ins,)))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.2))
+	model.add(Dense(layers))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.2))
+	model.add(Dense(n_out))
+	model.add(Activation('softmax'))
+
+	rms = RMSprop()
+	model.compile(loss='categorical_crossentropy', optimizer=rms)
+	model.fit(train_data, train_target,
+			batch_size=batch, nb_epoch=epoch,
+			show_accuracy=True, verbose=2,
+			validation_data=(test_data, test_target))
+	score = model.evaluate(test_data, test_target,
+			show_accuracy=True, verbose=1)
+
+	print('Test score:', score[0])
+	print('Test accuracy:', score[1])
+
+	return model.predict(test_data, verbose=0)
+
+def perft(out, target, helper, verbose=0):
+	print(os.linesep + 'Detailed prediction:')
+
+	for i in range(len(out)):
+		if verbose:
+			print('Expected: ' + str(numpy.argmax(target[i])) + ' ' +
+				'- Predicted: '+ str(numpy.argmax(out[i])) + ' ' +
+				'- Image: '    + str(helper[i][2]))
+
+def classify(perc, max_images):
+	inp = gen(perc, max_images)
+
+	train_data   = numpy.array(inp[0])
+	train_target = numpy.array(inp[1])
+	test_data    = numpy.array(inp[2])
+	test_target  = numpy.array(inp[3])
+	helper 		 = numpy.array(inp[4])
+
+	out = MultiLayerPerceptron(train_data, train_target, test_data, test_target)
+	perft(out, test_target, helper, verbose=1)
+
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print('Usage: python new_classifier.py percentage_for_train max_images')
+	else:
+		classify(float(sys.argv[1]), int(sys.argv[2]))
